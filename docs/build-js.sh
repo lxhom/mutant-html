7906c269dba87270f8352a68a308449a6680a473
lxhom <lxhomeis@gmail.com>
2022-11-27 20:34:12 +0100

    fix comment detection

---
+++ docs/build-js.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+shopt -s globstar
+
+rm assets/*.js assets/*.css assets/*.map
+
+(
+cd ..
+echo Compiling Svelte code...
+npm i
+npm run build
+echo Compiling as Userscript...
+node userscript_compile.cjs
+cp -r dist/* docs/
+)
+
+echo Done
